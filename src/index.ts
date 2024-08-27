c0c05b527d4885ede8be277205c811ff07ca225f
Potahog <potahog@gmail.com>
2024-08-27 12:25:41 +0900

    chore: implemente block and blockshape
    block shape 정의 후 block 클래스에서 구현
    
    deno에서 crypto를 사용하기 위한 Reference
    
    https://docs.deno.com/api/node/crypto/

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
-class Block{
-    constructor(private data: string) {}
-    static hello(){
-        return "hi"
+import * as crypto from "node:crypto";
+
+interface BlockShape {
+    prevHash: string;
+    height:number;
+    data: string;
+}
+
+class Block implements BlockShape{
+    
+    public hash: string;
+
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+
+    static calculateHash(prevHash:string, height:number, data:string): string {
+        const toHash = `${prevHash}${height}${data}`
+        return crypto.createHmac('sha256').update(toHash).digest('hex');
     }
 }
